f39f38c1295cb0f698bb25fdf986c38988e54636
Fabio Ranocchiari <fabio.ranocchiari@gmail.com>
2024-05-21 16:01:00 +0200

    update

--- chartProva.js
+++ chartProva.js
@@ -10,6 +10,22 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('wrongPercentage').innerText = `${wrongPercentage.toFixed(1)}%`;
     document.getElementById('correctAnswers').innerText = `${correctAnswers}/${totalQuestions} questions`;
     document.getElementById('wrongAnswers').innerText = `${wrongAnswers}/${totalQuestions} questions`;
+     var canvas = document.getElementById("resultsCanvas");
+      var context = canvas.getContext("2d");
+
+      // Draw the correct answers portion
+      context.beginPath();
+      context.arc(150, 150, 100, 0, Math.PI * 1.33);
+      context.lineWidth = 30;
+      context.strokeStyle = "#00e0e0";
+      context.stroke();
+
+      // Draw the wrong answers portion
+      context.beginPath();
+      context.arc(150, 150, 100, Math.PI * 1.33, 0);
+      context.lineWidth = 30;
+      context.strokeStyle = "#e000e0";
+      context.stroke();
     
     const ctx = document.getElementById('chart').getContext('2d');
     const chart = new Chart(ctx, {
@@ -30,26 +46,3 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
-
-var canvas = document.getElementById("resultsCanvas");
-var context = canvas.getContext("2d");
-
-// Draw the correct answers portion
-context.beginPath();
-context.arc(150, 150, 100, 0, Math.PI * 1.33);
-context.lineWidth = 30;
-context.strokeStyle = "#00e0e0";
-context.stroke();
-
-// Draw the wrong answers portion
-context.beginPath();
-context.arc(150, 150, 100, Math.PI * 1.33, 0);
-context.lineWidth = 30;
-context.strokeStyle = "#e000e0";
-context.stroke();
-
-
-function rateUs() {
-    alert('Thank you for your feedback!');
-}
-
