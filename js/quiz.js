210c746578aa462d7ed2ed43341b75c9c441ae5a
Nicolas P. <168541456+nikeniko@users.noreply.github.com>
2024-05-21 12:10:37 +0200

    base sito

--- js/quiz.js
+++ js/quiz.js
@@ -245,3 +245,15 @@ function showScore() {
 }
 
 startQuiz();
+
+let progressCircle = document.querySelector(".progress");
+let radius = progressCircle.r.baseVal.value;
+let circumference = radius * 2 * Math.PI;
+progressCircle.style.strokeDasharray = circumference;
+
+setProgress(75);
+
+function setProgress(percent) {
+  progressCircle.style.strokeDashoffset =
+    circumference - (percent / 100) * circumference;
+}
