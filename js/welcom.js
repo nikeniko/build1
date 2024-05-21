aa81975f1308c9b7ec674754a9be6efc6b5c0081
Petrov-Teo <168548498+Petrov-Teo@users.noreply.github.com>
2024-05-21 11:49:25 +0200

    File Js Funzione checkbox

--- js/welcom.js
+++ js/welcom.js
@@ -1,11 +1,22 @@
-const checkboxApp = document.querySelectorAll(
-  "input[type='checkbox]"
-);
+const checkboxApp = document.querySelectorAll(".Checkbox");
 
 const buttonCheck = document.querySelector(
   ".ButtonProceed"
 );
 
-buttonCheck.addEventListener("click", () => {
-  window.location.href = "../build1/quiz.html";
-});
+const nextPage = function (event) {
+  let isChecked = false;
+  checkboxApp.forEach((checkbox) => {
+    if (checkbox.checked) {
+      isChecked = true;
+    }
+  });
+  if (!isChecked) {
+    event.preventDefault();
+    alert("You must Accept the conditions");
+  } else {
+    return (href = "quiz.html");
+  }
+};
+
+buttonCheck.addEventListener("click", nextPage);
