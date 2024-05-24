eb2911512819a2bd608090f12272ecf4832a9b50
Petrov-Teo <168548498+Petrov-Teo@users.noreply.github.com>
2024-05-24 13:23:19 +0200

    aggiunto popup

--- js/feedback.js
+++ js/feedback.js
@@ -3,7 +3,8 @@ const starSVG = `
   <path d="M22.2044 1.55551C22.6143 0.569963 24.0104 0.569964 24.4203 1.55552L29.9874 14.9402C30.1602 15.3557 30.5509 15.6396 30.9994 15.6756L45.4494 16.834C46.5134 16.9193 46.9448 18.2471 46.1341 18.9415L35.1248 28.3722C34.7831 28.6649 34.6338 29.1242 34.7382 29.5619L38.1018 43.6626C38.3494 44.7009 37.2199 45.5215 36.309 44.9651L23.9379 37.4089C23.5538 37.1743 23.0709 37.1743 22.6868 37.4089L10.3157 44.9651C9.40478 45.5215 8.27528 44.7009 8.52295 43.6626L11.8865 29.5619C11.9909 29.1242 11.8416 28.6649 11.4999 28.3722L0.490575 18.9415C-0.320069 18.2471 0.111362 16.9193 1.17535 16.834L15.6253 15.6756C16.0738 15.6396 16.4645 15.3557 16.6374 14.9402L22.2044 1.55551Z" fill="#1a245e"/>
 </svg>`;
 
-const starRatingContainer = document.querySelector(".starsRating");
+const starRatingContainer =
+  document.querySelector(".starsRating");
 
 for (let i = 0; i < 10; i++) {
   starRatingContainer.innerHTML += starSVG;
@@ -35,13 +36,29 @@ stars.forEach((star, index) => {
     }
 
     let rating = index + 1;
+    let popup = document.querySelectorAll(".popup");
+    console.log(popup);
 
-    if (rating >= 1 && rating <= 3) {
-      alert("We're sorry to hear that your experience wasn't satisfactory. We'll strive to improve.");
-    } else if (rating >= 4 && rating <= 7) {
-      alert("Thank you for your feedback. We appreciate your opinion and will do our best to improve.");
-    } else if (rating >= 8 && rating <= 10) {
-      alert("Thank you for your positive feedback! We're glad you had a pleasant experience.");
+    let buttonp = document.getElementById("buttonPopup");
+    buttonp.addEventListener("click", closePopup);
+
+    function openPopup() {
+      popup.forEach((popup) => {
+        popup.classList.add("open-popup");
+      });
+    }
+    function closePopup() {
+      popup.forEach((popup) => {
+        popup.classList.remove("open-popup");
+      });
+    }
+
+    if (rating >= 1 && rating <= 6) {
+      openPopup();
+    } else if (rating >= 7 && rating <= 8) {
+      rating.addEventListener("click", openPopup);
+    } else if (rating >= 9 && rating <= 10) {
+      rating.addEventListener("click", openPopup);
     }
   });
 });
